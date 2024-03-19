c4c21038d6ebff42aee620eac478c40de7ab8877
Pete Bell <peter.bell@raspberrypi.org>
2024-03-19 15:45:31 +0000

    Artefact

--- en/code/quiz-time-complete/script.js
+++ en/code/quiz-time-complete/script.js
@@ -1,4 +1,7 @@
 var score = 0;
+var currentQ = 0;
+const questions = document.querySelectorAll(".q-container");
+
 function checkAnswer(question, correctAnswer, result) {
   let answer = document.querySelector(`input[name="${question}"]:checked`);
   document.querySelector(result).style.display = "block";
@@ -9,10 +12,29 @@ function checkAnswer(question, correctAnswer, result) {
       document.querySelector(result).innerText = "Correct";
       score++;
       document.querySelector("#scoreText").innerText = `Score: ${score}`;
+      nextQ();
     } else {
       document.querySelector(result).innerText = "Incorrect";
+      nextQ();
     }
   } else {
     document.querySelector(result).innerText = "Please select an answer";
   }
 }
+
+function nextQ() {
+  questions[currentQ].classList.add("fade-out");
+  setTimeout(() => {
+    if (currentQ < questions.length - 1) {
+      currentQ++;
+      questions[currentQ - 1].style.display = "none";
+      questions[currentQ].classList.add("slide-left");
+      questions[currentQ].style.display = "block";
+    } else {
+      document.querySelector("#scoreText").innerText = `Final score: ${score}`;
+    }
+  }, "2000");
+}
+
+questions[0].style.display = "block";
+questions[0].style.opacity = 1;
